f5df9eb06484887f399844b9c7a4c44c0fb5da04
Justin Mitchell <jmitchell31@g.ucla.edu>
2021-12-27 12:29:43 -0800

    Added component files for question and question list, starting some basic css for positioning

---
+++ client/src/components/Questions & Answers/components/Question-Answer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../QAstyles.css';
+
+const Question = function() {
+
+  return (
+    <div>
+      <span className="question">Q: this is an example question?</span>
+      <span className="answer">A: this is an example answer.</span>
+      <span className="from-user">User123, current_date</span>
+      <span className="helpful">Helpful? Yes (number of upvotes)</span>
+      <span className="report-button">Report</span>
+    </div>
+  )
+}
+
+export default Question;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import QuestionsAnswers from './components/Questions & Answers/index.jsx';
 
 class App extends React.Component {
   constructor(props) {
