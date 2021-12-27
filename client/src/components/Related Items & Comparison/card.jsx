58dbb60fe4e6f1b758fab7f16986afca0995179c
Gain Wang <gaincwang@gmail.com>
2021-12-27 11:37:47 -0800

    Implemented design for related item cards. Rudimentary styling and functionality

---
+++ client/src/components/Related Items & Comparison/card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+
+function Card(props) {
+  return (
+  <div className="related-item-card">
+    <img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1632925545-t-shirts-2021-jgt-ok-1632923427.jpg?crop=1xw:1xh;center,top&resize=768:*" />
+    <div className="product-details">
+      <div>Category</div>
+      <div>Expanded Product Name With Extra Text</div>
+      <div>Price</div>
+      <div>Review</div>
+    </div>
+  </div>)
+}
+
+export default Card;
