cae1ca3d638abce66e6654eacae34db93d0ab096
Edward Pak <edwardpak7@gmail.com>
2021-12-27 12:14:28 -0800

    fixed import statement in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import ProductInfo from './Overview/ProductInfo.jsx';
-=======
+import ProductInfo from './components/Overview/ProductInfo.jsx';
 import Card from './components/Related Items & Comparison/card.jsx';
->>>>>>> 686d87cada5334fa07913ce66fa144d501207167
 
 class App extends React.Component {
   constructor(props) {
@@ -19,12 +16,9 @@ class App extends React.Component {
     return (
       <>
       <h1>Hello world!</h1>
-<<<<<<< HEAD
       <ProductInfo />
-=======
       <Card />
       </>
->>>>>>> 686d87cada5334fa07913ce66fa144d501207167
     );
   }
 }
