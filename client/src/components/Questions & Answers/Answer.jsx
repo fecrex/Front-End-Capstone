3bc6294482dbd1c71b3ce5fd8ed1091b3b1d1bbe
Justin Mitchell <jmitchell31@g.ucla.edu>
2021-12-28 15:35:22 -0800

    Removed a from right corner of question label

--- client/src/components/Questions & Answers/Answer.jsx
+++ client/src/components/Questions & Answers/Answer.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Answer = function(props) {
   return (
     <div className="answer-content">
-       <span className="answer">{props.answer.body}</span>
-       <span className="from-user">{props.answer.answerer_name}, {props.answer.date}</span>
-       <span className="helpful-answer">Helpful? Yes ({props.answer.helpfulness})</span>
-       <span className="report-answer">Report</span>
+       <div className="answer">{props.answer.body}</div>
+       <div className="from-user">{props.answer.answerer_name}, {props.answer.date}</div>
+       <div className="helpful-answer">Helpful? Yes ({props.answer.helpfulness})</div>
+       <div className="report-answer">Report</div>
     </div>
   )
 }
--- client/src/components/Questions & Answers/Question-Answer.jsx
+++ client/src/components/Questions & Answers/Question-Answer.jsx
@@ -9,10 +9,10 @@ const Question = function(props) {
   return (
     <div className="individual-question">
       <input type="radio" id={props.id} className="accordion"/>
-      <label className="question-label" htmlFor={props.id}>Q: {props.question_body} </label>
+      <label className="question-label" htmlFor={props.id}>Q: {props.question_body} <div className="helpful-question">Helpful? Yes ({props.question_helpfulness})</div>
+      <div className="add-answer">| Add Answer</div> </label>
       {/* <span className="question">Q: {props.body}</span> */}
-      <div className="helpful-question">Helpful? Yes ({props.question_helpfulness})</div>
-      <div className="add-answer">| Add Answer</div>
+
       {/* <Answer answers={props.answers} answerIds={Object.keys(props.answers)}/> */}
       {count === 2 ? Object.keys(props.answers).slice(0,count).map((key, i) =>
         <Answer answer={props.answers[key]} key={i}/>
