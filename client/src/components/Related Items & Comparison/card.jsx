5023d540c22345a91e9132aec89b4794d169fb03
Gain Wang <gaincwang@gmail.com>
2021-12-28 11:34:56 -0800

    Added dynamic rendering of cards for related items, horizontal display for cards

--- client/src/components/Related Items & Comparison/card.jsx
+++ client/src/components/Related Items & Comparison/card.jsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState}from 'react';
 import axios from 'axios';
 
 function Card(props) {
-  return (
-  <div className="related-item-card">
-    <img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1632925545-t-shirts-2021-jgt-ok-1632923427.jpg?crop=1xw:1xh;center,top&resize=768:*" />
-    <div className="product-details">
-      <div>Category</div>
-      <div>Expanded Product Name With Extra Text</div>
-      <div>Price</div>
-      <div>Review</div>
-    </div>
-  </div>
-  )
+  const [relatedDetails, setDetails] = useState([]);
+  useEffect(() => {
+    if (props.relatedinfo) {
+      props.relatedinfo.forEach(element => {
+        axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${element}`, {
+          headers: {
+            Authorization: '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'
+          }
+        })
+        .then(data => {
+          setDetails(oldArray => [...oldArray, data.data]);
+        })
+      })
+    }
+  }, [props.relatedinfo]);
+
+  if (relatedDetails) {
+    return (
+      relatedDetails.map(item => {
+        return(
+          <>
+          <div className="related-item-card">
+          <img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1632925545-t-shirts-2021-jgt-ok-1632923427.jpg?crop=1xw:1xh;center,top&resize=768:*" />
+          <div className="product-details">
+            <div>{item.category}</div>
+            <div>{item.description}</div>
+            <div>{item.default_price}</div>
+            <div>Review</div>
+          </div>
+          </div>
+          </>
+        )
+      })
+    )
+  } else {
+    return <div>loading...</div>
+  }
+
+  // return (
+  // <div className="related-item-card">
+  //   <img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1632925545-t-shirts-2021-jgt-ok-1632923427.jpg?crop=1xw:1xh;center,top&resize=768:*" />
+  //   <div className="product-details">
+  //     <div>Category</div>
+  //     <div>Expanded Product Name With Extra Text</div>
+  //     <div>Price</div>
+  //     <div>Review</div>
+  //   </div>
+  // </div>
+  // )
 }
 
 export default Card;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import ProductInfo from './components/Overview/ProductInfo.jsx';
 import Card from './components/Related Items & Comparison/card.jsx';
 import ReviewList from './components/Ratings & Reviews/reviewList.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,13 +14,29 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    let x = [];
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40348/related', {
+      headers: {
+        Authorization: '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'
+      }
+    })
+    .then(results => {
+      this.setState({results: results.data});
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
       <>
       <h1>Hello world!</h1>
       <ProductInfo />
-      <QuestionsAnswers />
-      <Card />
+      <div className="related-container">
+        <Card relatedinfo={this.state.results}/>
+      </div>
       <ReviewList />
       </>
     );
