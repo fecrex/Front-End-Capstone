53b7374ed950f8f44be7fcfd70f1d16bcbb857b1
Edward Pak <edwardpak7@gmail.com>
2021-12-28 16:58:37 -0800

    started Image Gallery feature

---
+++ client/src/components/Overview/ImageGallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ImageGallery() {
+  return (
+    <div className='image-gallery'>
+      <img src='https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1632925545-t-shirts-2021-jgt-ok-1632923427.jpg?crop=1xw:1xh;center,top&resize=768:*'/>
+    </div>
+  )
+}
+
+export default ImageGallery;
