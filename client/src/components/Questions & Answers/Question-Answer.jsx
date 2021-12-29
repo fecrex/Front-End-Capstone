b1bc3696699af7b15fb9b73bf714afad495129d2
Justin Mitchell <jmitchell31@g.ucla.edu>
2021-12-28 17:23:29 -0800

    Added question helpfulness number to questions list

--- client/src/components/Questions & Answers/Question-Answer.jsx
+++ client/src/components/Questions & Answers/Question-Answer.jsx
@@ -3,17 +3,15 @@ import Answer from './Answer.jsx';
 
 const Question = function(props) {
 
+  // no real use for this, was using it to manipulate how many questions showed
   const [count, setCount] = useState(2);
   const [showMore, setShowMore] = useState(false);
 
   return (
     <div className="individual-question">
       <input type="radio" id={props.id} className="accordion" name="accordion_input"/>
-      <label className="question-label" htmlFor={props.id}>Q: {props.question_body} <div className="helpful-question">Helpful? Yes ({props.question_helpfulness})</div>
+      <label className="question-label" htmlFor={props.id}>Q: {props.question_body} <div className="helpful-question">Helpful? Yes ({props.q_helpfulness})</div>
       <div className="add-answer">| Add Answer</div> </label>
-      {/* <span className="question">Q: {props.body}</span> */}
-
-      {/* <Answer answers={props.answers} answerIds={Object.keys(props.answers)}/> */}
       <div className="answer-content">
         <p>
         {count === 2 ? Object.keys(props.answers).slice(0,count).map((key, i) => <Answer answer={props.answers[key]} key={i}/>
--- client/src/components/Questions & Answers/QuestionsList.jsx
+++ client/src/components/Questions & Answers/QuestionsList.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import Question from './Question-Answer.jsx';
 
 const QuestionsList = function(props) {
-
-  console.log('the props', props);
   return (
     <div className="questions-list" id="accordion">
       {props.productQA.map((question) =>
         <Question question_body={question.question_body} answers={question.answers} date={question.question_date} q_helpfulness={question.question_helpfulness} key={question.question_id} id={question.question_id}/>
       )}
-      {/* <Question body={props.productQA.question_body} askerName={props.productQA.asker_name} question_helpfulness={props.productQA.question_helpfulness} answers={props.productQA.answers}/> */}
     </div>
   )
 }
