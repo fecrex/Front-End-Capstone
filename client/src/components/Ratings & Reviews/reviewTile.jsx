fafe733ec008e0670fba87d05c4089a8fe2f6acb
Salar Malik <salarxmalik@gmail.com>
2021-12-30 23:09:28 -0800

    Formatting and Rendering Date Properly, Along with Star Ratings

--- client/src/components/Ratings & Reviews/reviewTile.jsx
+++ client/src/components/Ratings & Reviews/reviewTile.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import Rating from '@mui/material/Rating';
+import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
 
 const ReviewTile = function(props) {
+
+  var monthConverter= function(num) {
+    var monthsObj = {
+      '01': 'January',
+      '02': 'February',
+      '03': 'March',
+      '04': 'April',
+      '05': 'May',
+      '06': 'June',
+      '07': 'July',
+      '08': 'August',
+      '09': 'September',
+      '10': 'October',
+      '11': 'November',
+      '12': 'December'
+    };
+    return monthsObj[num.toString()];
+  }
+
   return (
     <div className="individual-review">
-      <div className="review-star-rating">Rating: {props.starRating}</div>
-      <div className="review-date">Review Date: {props.reviewDate}</div>
+      <Rating className="review-star-rating" defaultValue={props.starRating} precision={0.5} />
+      <div className="review-date">{monthConverter(props.reviewDate.substring(5, 7)) + ' ' + props.reviewDate.substring(8, 10) + ', ' + props.reviewDate.substring(0, 4)}</div>
       <div className="review-summary">Review Summary: {props.reviewSummary}</div>
       <div className="review-full-body">Full Review: {props.reviewBody}</div>
       {props.reviewImages.map((image) => {
@@ -13,10 +34,9 @@ const ReviewTile = function(props) {
       <div className="recommend-this-product">{(props.reviewRecommendation ? 'I Recommend this Product \u2713' : null)}</div>
       <div className="reviewer-name">Username: {props.reviewerName}</div>
       <div className="response-from-seller">{(props.reviewResponse ? 'Response from Seller: ' : null)}</div>
-      <div className="rating-helpfulness">Was this review helpful? {props.reviewHelpfulness}</div>
+      <div className="rating-helpfulness">Was this review helpful? Yes: {props.reviewHelpfulness}</div>
     </div>
   )
 }
 
 export default ReviewTile;
-
