35945f2b174297a05fc9146811b5f264c7d5fe7b
Edward Pak <edwardpak7@gmail.com>
2022-01-04 12:25:13 -0800

    building out add to cart functionality

--- client/src/components/Overview/SizeSelector.jsx
+++ client/src/components/Overview/SizeSelector.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
 
-function SizeSelector(props) {
-  return (
-    <select className='size-selector'>
-      <option>One Size Option</option>
-      <option>Two Size Option</option>
-      <option>Three Size Option</option>
-    </select>
-  );
+class SizeSelector extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sizes: [],
+      loaded: false
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.selected !== prevProps.selected) {
+      this.setState({
+        sizes: this.props.sizes,
+        loaded: true
+      })
+    }
+  }
+
+  render() {
+    return (
+
+
+      <select className='size-selector'>
+        <option>Select Size</option>
+
+
+
+        {/* {this.state.loaded ? Object.keys(this.state.sizes.skus).forEach((key) => {
+          var sku = this.state.sizes.skus[key]
+          var size = sku.size;
+          return <option>{size}</option>
+        }) : <option></option>} */}
+
+      </select>
+    );
+  }
 }
 
 export default SizeSelector;
