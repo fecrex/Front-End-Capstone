084fa321c4799a679f2d3b7d319fd95448664e3b
Salar Malik <salarxmalik@gmail.com>
2022-01-07 01:38:37 -0800

    Live Data in Review Breakdown Rendering

--- client/src/components/Ratings & Reviews/reviewRatingBreakdown.jsx
+++ client/src/components/Ratings & Reviews/reviewRatingBreakdown.jsx
@@ -7,21 +7,77 @@ class ReviewRatingBreakdown extends React.Component {
     this.state = {
       numberOfReviews: null,
       averageRating: null,
-      percentRecommend: null
+      percentRecommend: null,
+      loaded: false
     }
   }
 
+  componentDidMount() {
+    this.getReviewCount();
+    this.getAverageRating();
+    this.getPercent();
+  }
 
+  getReviewCount = function() {
+    var reviewValues = Object.values(this.props.reviewRatings);
+    var sumStrNums = function(arr) {
+      var sum = 0;
+      for (var i = 0; i < arr.length; i++) {
+        sum += Number(arr[i]);
+      }
+      return sum;
+    }
+    this.setState({
+      numberOfReviews: sumStrNums(reviewValues)
+    })
+  }
+
+  getAverageRating = function() {
+    var reviewValues = Object.values(this.props.reviewRatings);
+    var getAvg = function(arr) {
+      var total = 0;
+      var totalValue = 0;
+      var result = 0;
+      for (var i = 0; i < arr.length; i++) {
+        total += Number(arr[i]);
+      }
+      for (var j = 0; j < arr.length; j++) {
+        var currentNum = Number(arr[j])
+        var multiplier = j + 1;
+        totalValue += (currentNum * multiplier);
+      }
+      result = Math.round((totalValue/total) * 10) / 10;
+      return result;
+    }
+    this.setState({
+      averageRating: getAvg(reviewValues),
+      loaded: true
+    })
+  }
+
+  getPercent = function() {
+    var getRecRating = function(obj) {
+      var trueVal = Number(obj.true);
+      var falseVal = Number(obj.false);
+      var totalVal = trueVal + falseVal;
+      return Math.round((trueVal/totalVal) * 100);
+    }
+    this.setState({
+      percentRecommend: getRecRating(this.props.reviewRecommendations)
+    })
+  }
 
   render () {
     return (
       <div>
-        <div>{3.5 + '  '}
-          <Rating className="review-star-breakdown" defaultValue={3.5} precision={0.25} readOnly />
-        <div>{5} Total Reviews</div>
+        <div>{this.state.averageRating + '  '}
+          {this.state.loaded ?
+          <Rating className="review-star-breakdown" defaultValue={this.state.averageRating} precision={0.25} readOnly />
+          : null}
+        <div>{this.state.numberOfReviews} Total Reviews</div>
         </div>
       <div>Rating Breakdown</div>
-      <div>{30}% recommend this product</div>
+      <div>{this.state.percentRecommend}% recommend this product</div>
     </div>
     )
   }
