4ef1ebf1fac79bb0900f5faaf4c47fc01186a5d9
Edward Pak <edwardpak7@gmail.com>
2022-01-08 11:15:02 -0800

    app optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const SRC_DIR = path.join(__dirname, "/client/src");
 const DIST_DIR = path.join(__dirname, "/client/dist");
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
+  plugins: [new CompressionPlugin()],
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
